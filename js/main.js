21c9f7c783b2baab49a63023f0e47b381cf665bf
moamel hashim <muamal1997@gmail.com>
2022-02-14 13:12:21 -0800

    added an input listener for photoURL to update the src attribute

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+const $photoURL = document.querySelector('#image');
+$photoURL.addEventListener('input', inputHandler);
+function inputHandler(event) {
+  const $img = document.querySelector('.image');
+  $img.setAttribute('src', $photoURL.value);
+  if ($photoURL.value === '') {
+    $img.setAttribute('src', './images/placeholder-image-square.jpg');
+  }
+}
