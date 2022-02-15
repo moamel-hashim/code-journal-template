9b8e85f477e4566785fef39a8d25932442aa76f7
moamel hashim <muamal1997@gmail.com>
2022-02-14 17:52:25 -0800

    completed some tasks as well as added an anchor to the top of the page

--- js/main.js
+++ js/main.js
@@ -24,29 +24,14 @@ function formHandler(event) {
   $form.reset();
 }
 
-const $ul = document.querySelector('ul');
+const $ul = document.querySelector('.ul');
 function domTree(entry) {
   const $li = document.createElement('li');
+  $li.setAttribute('class', 'margin');
   $ul.appendChild($li);
-  const $container = document.createElement('div');
-  $container.setAttribute('class', 'container');
-  $ul.appendChild($container);
-  const $row = document.createElement('div');
-  $row.setAttribute('class', 'row justify-content-space-between align-items-center');
-  $container.appendChild($row);
-  const $divEntries = document.createElement('div');
-  $row.appendChild($divEntries);
-  const $h1 = document.createElement('h1');
-  $h1.textContent = 'Entries';
-  $divEntries.appendChild($h1);
-  const $divButton = document.createElement('div');
-  $row.appendChild($divButton);
-  const $button = document.createElement('button');
-  $button.textContent = 'new';
-  $divButton.appendChild($button);
   const $secondRow = document.createElement('div');
-  $secondRow.setAttribute('class', 'row');
-  $container.appendChild($secondRow);
+  $secondRow.setAttribute('class', 'row container');
+  $li.appendChild($secondRow);
   const $divHalf = document.createElement('div');
   $divHalf.setAttribute('class', 'column-half');
   $secondRow.appendChild($divHalf);
@@ -54,7 +39,7 @@ function domTree(entry) {
   $imgContainer.setAttribute('class', 'img-container');
   $divHalf.appendChild($imgContainer);
   const $img = document.createElement('img');
-  $img.setAttribute('src', './images/placeholder-image-square.jpg');
+  $img.setAttribute('src', entry.image);
   $img.setAttribute('class', 'border-radius');
   $imgContainer.appendChild($img);
   const $secondDivHalf = document.createElement('div');
@@ -64,12 +49,17 @@ function domTree(entry) {
   $textContainer.setAttribute('class', 'text-container');
   $secondDivHalf.appendChild($textContainer);
   const $h2 = document.createElement('h2');
-  $h2.textContent = 'Ada lovelace';
+  $h2.textContent = entry.title;
   $textContainer.appendChild($h2);
   const $p = document.createElement('p');
-  $p.textContent = 'hello';
+  $p.textContent = entry.text;
   $textContainer.appendChild($p);
   return $li;
 }
 
-domTree();
+window.addEventListener('DOMContentLoaded', handleDomContent);
+function handleDomContent(event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    $ul.appendChild(domTree(data.entries[i]));
+  }
+}
